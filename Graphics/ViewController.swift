018cc76c6e0969406d307534aa790d7c6396a6df
Mindaugas Jucius <ciamusu1@gmail.com>
2015-08-21 14:33:13 +0300

    added labels to graph

--- Graphics/ViewController.swift
+++ Graphics/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
     
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -38,6 +45,70 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+    }
+    
+    @IBAction func counterViewTap(gesture:UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            
+            //hide Graph
+            UIView.transitionFromView(graphView,
+                toView: counterView,
+                duration: 1.0,
+                options: [.TransitionFlipFromLeft, .ShowHideTransitionViews],
+                completion:nil)
+        } else {
+            
+            //show Graph
+            UIView.transitionFromView(counterView,
+                toView: graphView,
+                duration: 1.0,
+                options: [.TransitionFlipFromLeft, .ShowHideTransitionViews],
+                completion: nil)
+            setupGraphDisplay()
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
+    
+    func setupGraphDisplay() {
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        maxLabel.text = "\(graphView.graphPoints.maxElement())"
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, combine: +)
+            / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        let calendar = NSCalendar.currentCalendar()
+        let componentOptions:NSCalendarUnit = .Weekday
+        let components = calendar.components(componentOptions,
+            fromDate: NSDate())
+        var weekday = components.weekday
+        
+        let days = ["M", "T", "W", "T", "F", "S", "S"]
+        days.reverse()
+        //5 - set up the day name labels with correct day
+        for i in (1...days.count) {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                labelView.text = days[weekday--]
+                if weekday < 0 {
+                    weekday = days.count - 1
+                }
+            }
+        }
     }
     
 }
